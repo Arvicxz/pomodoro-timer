123c47cc7ab9260341db1a6b3b7fa8255c43a8eb
Arthur Victor <arthurvictor1404@gmail.com>
2026-03-17 18:03:17 -0300

    feat: First commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const startBtn = document.getElementById("start");
+const pauseBtn = document.getElementById("pause");
+const resetBtn = document.getElementById("reset");
+const timerDisplay = document.querySelector(".timer h2");
+
